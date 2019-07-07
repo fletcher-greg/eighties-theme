e99f7a5048127674366e3b55326b227bd5a9659c
fletcher-greg <fletcher.web.dev.one@gmail.com>
2019-07-07 23:04:10 +0200

    Updates

---
+++ precache-manifest.dd3f79b837c1933040c827e17b868802.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3849a3505c05eddb8c496f2c87ca8db3",
+    "url": "/fletcher-greg/eighties-theme/index.html"
+  },
+  {
+    "revision": "6bdedd2071f0b0716479",
+    "url": "/fletcher-greg/eighties-theme/static/css/main.125e7f26.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/fletcher-greg/eighties-theme/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "6bdedd2071f0b0716479",
+    "url": "/fletcher-greg/eighties-theme/static/js/main.15ea3d9b.chunk.js"
+  },
+  {
+    "revision": "2f873c867fdd9b8d695e",
+    "url": "/fletcher-greg/eighties-theme/static/js/runtime~main.3b6d6ae1.js"
+  }
+]);
